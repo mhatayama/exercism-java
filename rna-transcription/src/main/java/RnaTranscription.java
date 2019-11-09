5bf39cfd785dd3d6ba0416674d778d7463bec6f5
Masato Hatayama <masato.hatayama@gmail.com>
2019-11-10 02:50:55 +0900

    completed

---
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+class RnaTranscription {
+    private static final Map<String, String> TRANS;
+    static {
+        TRANS = new HashMap<>();
+        TRANS.put("G", "C");
+        TRANS.put("C", "G");
+        TRANS.put("T", "A");
+        TRANS.put("A", "U");
+    }
+
+    String transcribe(String dnaStrand) {
+        if (dnaStrand.isEmpty())
+            return "";
+        return Arrays.stream(dnaStrand.split("")).map(s ->
+            TRANS.get(s)).collect(Collectors.joining());
+    }
+
+}
