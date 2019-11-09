0b34bbd655a35eeed528c7fa757a4c6cd0cb1678
Masato Hatayama <masato.hatayama@gmail.com>
2019-11-10 02:59:47 +0900

    completed

---
+++ raindrops/src/main/java/RaindropConverter.java
@@ -0,0 +1,15 @@
+class RaindropConverter {
+
+    String convert(int number) {
+        StringBuilder result = new StringBuilder();
+        if (number % 3 == 0)
+            result.append("Pling");
+        if (number % 5 == 0)
+            result.append("Plang");
+        if (number % 7 == 0)
+            result.append("Plong");
+
+        return result.length() == 0 ? String.valueOf(number) : result.toString();
+    }
+
+}
